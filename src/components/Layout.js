9c8693a9aa47c3f72ad4fc48aeebd56abb22390b
Karla Vargas <kleandrav@gmail.com>
2021-05-20 09:12:50 +0800

    Style navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Navbar from './Navbar';
+import {Link} from 'gatsby';
 import '../css/global.css';
-import '../css/layout.css';
+import * as style from '../css/layout.module.css';
 
 function Layout({children}) {
     return (
         <div>
-            <Navbar/>
+            <nav className={style.navbar}>
+                
+                <Link to="/" className={style.brand}>
+                    <h2>KV</h2>
+                </Link>
+                <div className={style.links}>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/projects">Projects</Link>
+                </div>
+            </nav>
+
             {children}
+
             <footer>
                 Copyright 2021 KV
             </footer>
